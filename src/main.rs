e2361074195bba76d3a06d2d9b970fd19a5133bd
Sébastien <sebastien@shyrogan.fr>
2024-12-07 21:27:32 +0100

    feat(otel): `/metrics` for prometheus

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,28 @@ pub mod config;
 pub mod translation;
 
 use axum::Router;
+use axum_otel_metrics::HttpMetricsLayerBuilder;
 use config::Config;
 use open_api::ApiDoc;
 use utoipa::OpenApi;
 use utoipa_axum::router::OpenApiRouter;
-use utoipa_scalar::Scalar;
+use utoipa_scalar::{Scalar, Servable};
 
 pub fn router() -> Router {
+    let metrics = HttpMetricsLayerBuilder::new()
+        .with_service_name(env!("CARGO_PKG_NAME").to_string())
+        .with_service_version(env!("CARGO_PKG_VERSION").to_string())
+        .build();
+
     let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
         .nest("/api/v1/subject", subject::router())
         .nest("/api/v1/translation", translation::router())
         .split_for_parts();
 
-    router.merge(Scalar::new(api))
+    router
+        .merge(Scalar::with_url("/api/v1/scalar", api))
+        .merge(metrics.routes())
+        .layer(metrics)
 }
 
 #[tokio::main]
